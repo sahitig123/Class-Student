4161a21118734ec44070f2353c0839996299c236
sahitig123 <89743954+sahitig123@users.noreply.github.com>
2021-10-23 12:49:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var s1, s2
+function setup() {
+  createCanvas(400,400);
+s1 = new Student("hailey", 16, 10)
+s2 = new Student("Johnny", 12, 7)
+s1.display()
+s2.display()
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
